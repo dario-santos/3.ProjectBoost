3680fd097e14cadf10d8cf7e3bd05fe1d99cf796
Dário Santos <dariovfsantos@gmail.com>
2018-04-13 15:35:02 +0100

    Added space ship
    Added space ship with singular colors

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        ProcessInput();
+	}
+
+
+    //Process Input
+    private void ProcessInput()
+    {
+        //Pode rodar e impulsionar ao mesmo tempo
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            print("Rotate A");
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            print("Rotate D");
+        }
+
+        if(Input.GetKey(KeyCode.Space)) //Thrust == impulso
+        {
+            print("Thrust Space");
+        }
+
+    }
+}
