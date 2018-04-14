c53e185b6f31804c82b004cd13adabdd7faf7c4f
Dário Santos <dariovfsantos@gmail.com>
2018-04-14 22:48:08 +0100

    Particles and Levels
    Added:
         Particles
         More sounds
         game manager
         manual movement of objects

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+
+    [SerializeField] Vector3 move;
+    [Range(0, 1)] [SerializeField] float moveFactor;
+
+    Vector3 startPosition;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        startPosition = transform.position;
+	}
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        transform.position = startPosition + (move * moveFactor);
+
+	}
+}
--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,62 +1,164 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
+[DisallowMultipleComponent]
 public class Rocket : MonoBehaviour {
+
+
+    [SerializeField] float rcsRotation = 250f;
+    [SerializeField] float rcsThrut = 50f;
+
+    [SerializeField] AudioClip m_mainEngine;
+    [SerializeField] AudioClip m_dying;
+    [SerializeField] AudioClip m_win;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem dyingParticles;
+    [SerializeField] ParticleSystem winParticles;
 
     //Game data
     Rigidbody rigidBody;
     AudioSource audioSource;
 
+    enum State {Alive, Dying, Trascending };
+    State state = State.Alive;
+
+
 	// Use this for initialization
 	void Start()
     {
-
+        
         //Não temos que dizer como vamos buscar o rigidbod, apenas que o temos que ir buscar
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
 
     }
 	
+
 	// Update is called once per frame
 	void Update()
     {
-        ProcessInput();
+        //ToDo: Parar som quando morre
+        if (state == State.Alive)
+        {
+            RespondingToThrutInput();
+            RespondingToRotateInput();
+        }
+        
 	}
 
 
-    //Process Input
-    private void ProcessInput()
+    private void OnCollisionEnter(Collision collision)
     {
-        //Pode rodar e impulsionar ao mesmo tempo
+        if(state != State.Alive)
+            return;
 
-        if(Input.GetKey(KeyCode.A))
+        switch(collision.gameObject.tag)
         {
-            transform.Rotate(Vector3.forward);
+            case "Friendly":
+                //Don't do nothing
+                break;
+
+            case "Finish":
+                StartSucessSequence();
+                break;
+
+            default:
+                StartDeathSequence();
+                break;
         }
-        else if(Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward);
-        }
+    }
+
+
+    private void StartSucessSequence()
+    {
+        audioSource.Stop();
+        audioSource.PlayOneShot(m_win);
+
+        winParticles.Play();
+
+        state = State.Trascending;
+        Invoke("LoadNextScene", 1f); //Carregar nível 2
+    }
+
+
+    private void StartDeathSequence()
+    {
+        audioSource.Stop();
+        audioSource.PlayOneShot(m_dying);
+
+        dyingParticles.Play();
+        state = State.Dying;
+        Invoke("LoadFirstScene", 1f);//Perma death
+    }
+
+
+    private void LoadNextScene()
+    {
+
+        winParticles.Stop();
+
+        SceneManager.LoadScene(1);
+        state = State.Alive;
+
+    }
+
+
+    private void LoadFirstScene()
+    {
+        dyingParticles.Stop();
+
+        SceneManager.LoadScene(0);
+        state = State.Alive;
+    }
+
+    private void RespondingToThrutInput()
+    {
 
         if (Input.GetKey(KeyCode.Space)) //Thrust == impulso
         {
-
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
-            rigidBody.AddRelativeForce(Vector3.up);
-
+            ApplyThrut();
         }
         else
         {
             if (audioSource.isPlaying)
             {
                 audioSource.Stop();
+                mainEngineParticles.Stop();
             }
-
         }
+    }
+
+    private void ApplyThrut()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * rcsThrut);
+
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(m_mainEngine);
+        }
+        mainEngineParticles.Play();
+    }
+
+
+    //Process Input
+    private void RespondingToRotateInput()
+    {
+        //Pode rodar e impulsionar ao mesmo tempo
+        rigidBody.freezeRotation = true; //Take manual control of rotation
+
+        float rotationThisFrame = rcsRotation * Time.deltaTime;
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; //Resume physics control
+
 
     }
 }
